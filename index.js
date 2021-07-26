9d0af194545ab29edb26204e45c76d2f9fbe7a15
Seth Falco <seth@falco.fun>
2021-07-26 18:04:08 +0200

    fix: use const instead of var

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var fs = require("fs");
-var Handlebars = require("handlebars");
+const fs = require("fs");
+const Handlebars = require("handlebars");
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	const template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
