d9a9407f74f3a79428bc23c75f9c06fd8257afe5
Seth Falco <seth@falco.fun>
2022-06-27 16:31:14 +0100

    chore: rename resume.template to resume.handlebars for code completion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Handlebars = require("handlebars");
 
 function render(resume) {
 	const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	const template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	const template = fs.readFileSync(__dirname + "/resume.handlebars", "utf-8");
 
 	return Handlebars.compile(template)({
 		css: css,
