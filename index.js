c67a66610052a0f299226fb88ef5785c78f22177
Seth Falco <seth@falco.fun>
2023-12-04 00:49:46 +0000

    feat: minify html and various improvements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Handlebars = require("handlebars");
 const { marked } = require("marked");
+const { minify } = require('html-minifier');
 
 /**
  * Custom renderer for marked, namely to disable unwanted features.
@@ -34,6 +35,20 @@ const renderer = {
 
 marked.use({ renderer });
 
+/**
+ * Plugins to enable to minify HTML after generating from the template.
+ */
+const minifyOptions = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  decodeEntities: true,
+  minifyCSS: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  sortAttributes: true,
+  sortClassName: true,
+};
+
 Handlebars.registerHelper("date", (body) => {
   if (!body) {
     return "Present"
@@ -54,21 +69,32 @@ Handlebars.registerHelper("markdown", (body) => {
   return marked.parse(body);
 });
 
+Handlebars.registerHelper("link", (body) => {
+  const parsed = new URL(body);
+  const host = (parsed.host.startsWith('www.')) ? parsed.host.substring(4) : parsed.host;
+  return `<a href="${body}">${host}</a>`;
+});
+
+/**
+ * @param {Object} resume
+ * @returns {string}
+ */
 function render(resume) {
   const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
   const template = fs.readFileSync(__dirname + "/resume.handlebars", "utf-8");
   const { profiles } = resume.basics;
 
   if (Array.isArray(profiles)) {
-    const twitter = profiles.find((profile) => {
-      return profile.network.toLowerCase().includes('twitter');
+    const xTwitter = profiles.find((profile) => {
+      const name = profile.network.toLowerCase();
+      return name === 'x' || name === 'twitter';
     });
 
-    if (twitter) {
-      let { username, url } = twitter;
+    if (xTwitter) {
+      let { username, url } = xTwitter;
 
       if (!username && url) {
-        const match = url.match(/https?:\/\/.+?\/([\w]{1,15})/);
+        const match = url.match(/https?:\/\/.+?\/(\w{1,15})/);
 
         if (match.length == 2) {
           username = match[1];
@@ -80,17 +106,32 @@ function render(resume) {
       }
 
       resume.custom = {
-        twitterHandle: username
+        xTwitterHandle: username
       }
     }
   }
 
-  return Handlebars.compile(template)({
-    css: css,
+  const primaryLanguage = resume.languages && resume.languages[0].language;
+
+  if (primaryLanguage) {
+    Handlebars.registerHelper("lang", (body) => {
+      const languageNames = new Intl.DisplayNames([primaryLanguage], {
+        type: 'language',
+
+      });
+
+      return languageNames.of(body);
+    });
+  }
+
+  const html = Handlebars.compile(template)({
+    css,
     resume
   });
+
+  return minify(html, minifyOptions);
 }
 
 module.exports = {
-  render: render
+  render
 };
