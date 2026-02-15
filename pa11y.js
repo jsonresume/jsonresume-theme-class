d1cfd428ec5f79cb9129644642353f455fee7079
Seth Falco <seth@falco.fun>
2026-02-15 21:39:14 +0000

    chore: add pa11y and adhere to wcag aaa

---
+++ pa11y.js
@@ -0,0 +1,85 @@
+import { styleText } from 'node:util';
+import pa11y from 'pa11y';
+import puppeteer from 'puppeteer';
+
+/**
+ * @typedef {'light'|'dark'} ColorScheme
+ *
+ * @typedef {object} TestCase
+ * @property {string} name
+ * @property {string} url
+ * @property {ColorScheme} colorScheme
+ */
+
+const PAGE_URL = 'http://localhost:8080/resume.html';
+
+/** @type {TestCase[]} */
+const TEST_CASES = [
+  {
+    name: 'Light Mode (default)',
+    url: PAGE_URL,
+    colorScheme: 'light'
+  },
+  {
+    name: 'Dark Mode',
+    url: PAGE_URL,
+    colorScheme: 'dark'
+  },
+];
+
+/**
+ * @param {import('puppeteer').Browser} browser
+ * @param {TestCase} testCase
+ * @returns {Promise<any>}
+ */
+const pa11yRunner = async (browser, testCase) => {
+  const page = await browser.newPage();
+  await page.emulateMediaFeatures([
+    { name: 'prefers-color-scheme', value: testCase.colorScheme }
+  ]);
+
+  const results = await pa11y(testCase.url, {
+    standard: 'WCAG2AAA',
+    browser,
+    page,
+  });
+
+  await page.close();
+  return results;
+};
+
+const browser = await puppeteer.launch();
+const results = await Promise.all(TEST_CASES.map(async (testCase) => ({
+  name: testCase.name,
+  result: await pa11yRunner(browser, testCase),
+})));
+
+await browser.close();
+
+let failed = false;
+
+for (const { name, result } of results) {
+  if (result.issues.length === 0) {
+    continue;
+  }
+
+  failed = true;
+
+  for (const issue of result.issues) {
+    console.error(
+      '%s > %s\n%s %s\n%s %s\n%s %s\n',
+      styleText(['blue', 'bold'], name),
+      styleText(['red', 'bold'], issue.code),
+      styleText('yellow', 'Message:'),
+      issue.message,
+      styleText('yellow', 'Context:'),
+      styleText(['gray', 'italic'], issue.context),
+      styleText('yellow', 'Selector:'),
+      styleText('gray', issue.selector),
+    );
+  }
+}
+
+if (failed) {
+  process.exitCode = 1;
+}
